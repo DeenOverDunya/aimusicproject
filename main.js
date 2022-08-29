14d81a49a4f11eb5156413be7a5df3d5e2234741
DeenOverDunya <104869003+DeenOverDunya@users.noreply.github.com>
2022-08-29 20:54:32 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+peter_pan="";
+harry_potter="";
+
+
+
+function setup()
+{
+canvas=createCanvas(600,500);
+canvas.center();
+
+video = createCapture(VIDEO);
+video.hide();
+}
+
+
+function draw(){
+    image(video,0,0,600,500);
+}
+
